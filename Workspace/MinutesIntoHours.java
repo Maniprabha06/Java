5fa23bbb670887fa21aff161640ff5a62fb938e3
Maniprabha S <108254371+Maniprabha06@users.noreply.github.com>
2022-11-06 12:03:17 +0530

    Create MinutesIntoHours.java

---
+++ Workspace/MinutesIntoHours.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[]args)
+    {
+        int a=120;
+        int h;
+        h=a/60;
+        System.out.println(a +" minutes in hours is " +h);
+    }
+}
