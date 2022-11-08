57bb0d980d7cab49f19923b7b2eed512d23c1f32
Maniprabha S <108254371+Maniprabha06@users.noreply.github.com>
2022-11-08 21:33:56 +0530

    Factorial

---
+++ Workspace/FactorialOfaNumber.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[]args)
+    {
+        int a=3;
+        int b=1;
+        for(int i=1;i<=a;i++)
+        {
+            b=b*i;
+        }
+        System.out.print("Factorial of "+a+" is "+b);
+    }
+}
